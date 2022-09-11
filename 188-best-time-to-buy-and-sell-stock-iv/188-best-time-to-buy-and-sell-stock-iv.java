d933a68ca46866a7c148f10503c0b45fef06ef58
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-09-11 13:24:43 +0530

    Time: 4 ms (66.00%), Space: 41.6 MB (92.16%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,22 @@
 class Solution {
     public int maxProfit(int k, int[] prices) {
         int n = prices.length;
-        int[][][] dp = new int[n][k+1][2];
-        return checkProfit(0, 0, k, prices, dp);
+        int[][] dp = new int[n][2*k+1];
+        return checkProfit(0, 0, 2*k, prices, dp);
     }
-    private int checkProfit(int i, int buy, int numTrans, int[] prices, int[][][] dp){
-        if(i >= prices.length || numTrans == 0) return 0;
+    private int checkProfit(int i, int numTrans, int k, int[] prices, int[][] dp){
+        if(i >= prices.length || numTrans == k) return 0;
         
-        if(dp[i][numTrans][buy] != 0) return dp[i][numTrans][buy];
+        if(dp[i][numTrans] != 0) return dp[i][numTrans];
         
-        int profit = checkProfit(i+1, buy, numTrans, prices, dp);
-        if(buy == 0){
-            profit = Math.max(profit, -prices[i] + checkProfit(i+1, 1, numTrans, prices, dp));
+        int profit = checkProfit(i+1, numTrans, k, prices, dp);
+        if(numTrans%2 == 0){
+            profit = Math.max(profit, -prices[i] + checkProfit(i+1, numTrans+1, k, prices, dp));
         } else{
-            profit = Math.max(profit, prices[i] + checkProfit(i+1, 0, numTrans-1, prices, dp));
+            profit = Math.max(profit, prices[i] + checkProfit(i+1, numTrans+1, k, prices, dp));
         }
         
         
-        return dp[i][numTrans][buy] = profit;
+        return dp[i][numTrans] = profit;
     }
 }
