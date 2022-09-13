000e183e53b590f9cbea3d2477fd5d9fbb43c542
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-09-13 14:57:33 +0530

    Time: 4 ms (64.47%), Space: 49.8 MB (49.35%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,6 +1,6 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int num1 = -1, num2 = -1, c1 = 0, c2 = 0, n = nums.length;
+        int num1 = 0, num2 = 0, c1 = 0, c2 = 0, n = nums.length;
         for(int num : nums){
             if(num == num1) c1++;
             else if(num == num2) c2++;
