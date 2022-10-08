95597560e687b73faa55a1ec3d6019071239ddf5
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-08 18:46:10 +0530

    Time: 20 ms (42.19%), Space: 44.5 MB (35.97%) - LeetHub

--- 38-count-and-say/38-count-and-say.java
+++ 38-count-and-say/38-count-and-say.java
@@ -3,21 +3,16 @@ class Solution {
         if(n == 1) return "1";
         String s = "1";
         for(int i = 1; i < n; ++i){
-            int count = 0, num = 0;
+            int count = 0, num = s.charAt(0)-'0';
             StringBuilder temp = new StringBuilder();
             for(int j = 0; j < s.length(); ++j){
                 int digit = s.charAt(j) - '0';
-                if(j == 0){
+                if(digit == num){
+                    count++;
+                } else{
+                    temp.append(count + "" + num);
                     count = 1;
                     num = digit;
-                } else{
-                    if(digit == num){
-                        count++;
-                    } else{
-                        temp.append(count + "" + num);
-                        count = 1;
-                        num = digit;
-                    }
                 }
             }
             temp.append(count + "" + num);
