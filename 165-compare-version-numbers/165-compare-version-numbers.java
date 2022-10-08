cc700bdc882b8b04148f279af8cfdeffd1ad20ba
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-08 17:42:56 +0530

    Time: 4 ms (14.09%), Space: 40.6 MB (86.59%) - LeetHub

--- 165-compare-version-numbers/165-compare-version-numbers.java
+++ 165-compare-version-numbers/165-compare-version-numbers.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int compareVersion(String version1, String version2) {
         String[] v1 = version1.split("[.]");
@@ -15,6 +16,26 @@ class Solution {
             }
             if(num1 < num2) return -1;
             else if(num2 < num1) return 1;
+        }
+        return 0;
+    }
+}
+*/
+
+
+class Solution {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("[.]");
+        String[] v2 = version2.split("[.]");
+        int n = v1.length, m = v2.length;
+        int max = Math.max(n, m);
+        for(int i = 0; i < max; ++i){
+            int num1 = i < n ? Integer.parseInt(v1[i]) : 0;
+            int num2 = i < m ? Integer.parseInt(v2[i]) : 0;
+            
+            if(num1 < num2) return -1;
+            else if(num2 < num1) return 1;
+            
         }
         return 0;
     }
