697f148517e9f9ef09925fb262708ca896a14645
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-08 18:12:30 +0530

    Time: 4 ms (6.83%), Space: 42.9 MB (6.83%) - LeetHub

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,5 +1,38 @@
+/*
 class Solution {
     public int strStr(String haystack, String needle) {
         return haystack.indexOf(needle);
     }
 }
+*/
+class Solution {
+    public int strStr(String haystack, String needle) {
+        String st = needle + "$" + haystack;
+        int len = st.length();
+        int[] lps = new int[len];
+        computeLPS(lps, st, len);
+        int nLen = needle.length();
+        for(int i = nLen+1; i < len; ++i){
+            if(lps[i] == nLen) return i - 2*nLen;
+        }
+        return -1;
+        
+    }
+    private void computeLPS(int[] lps, String st, int slen){
+        int len = 0, i = 1;
+        while(i < slen){
+            if(st.charAt(i) == st.charAt(len)){
+                lps[i] = len+1;
+                len++;
+                i++;
+            } else{
+                if(len != 0) len = lps[len-1];
+                else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+}
+
