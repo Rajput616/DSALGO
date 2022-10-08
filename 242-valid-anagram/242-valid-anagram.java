603dc66082f84f0fbe498d8f49032b1ae320a06c
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-08 17:21:49 +0530

    Time: 5 ms (82.04%), Space: 43.1 MB (83.58%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+        int[] chars = new int[26];
+        for(char ch : s.toCharArray()){
+            chars[ch-'a']++;
+        }
+        for(char ch : t.toCharArray()){
+            chars[ch-'a']--;
+            if(chars[ch-'a'] < 0) return false;
+        }
+        return true;
+    }
+}
