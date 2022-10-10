412046aea3686f12f17f35b54e90cdffb2c855b2
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-10 18:11:40 +0530

    Added solution - LeetHub

---
+++ Minimum Multiplications to reach End - GFG/minimum-multiplications-to-reach-end.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// Position this line where user code will be pasted.
+
+class GFG {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T-- > 0) {
+            int n = sc.nextInt();
+            int[] a = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                a[i] = sc.nextInt();
+            }
+
+            int start = sc.nextInt();
+            int end = sc.nextInt();
+
+            Solution ob = new Solution();
+            int ans = ob.minimumMultiplications(a, start, end);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    int minimumMultiplications(int[] arr, int start, int end) {
+        int len = arr.length;
+        
+        int[] dist = new int[100000];
+        Arrays.fill(dist, 100000);
+        dist[start] = 0;
+        Queue<int[]> q = new LinkedList();
+        q.offer(new int[]{start, 0});
+        while(!q.isEmpty()){
+            int[] front = q.poll();
+            int num = front[0];
+            int steps = front[1];
+            
+            for(int i = 0; i < len; ++i){
+                int mul = (num * arr[i]) % 100000;
+                if(steps + 1 < dist[mul]) {
+                    dist[mul] = steps+1;
+                    if(mul == end) return steps+1;
+                    q.offer(new int[]{mul, steps+1});
+                    
+                }
+            }
+        }
+        return -1;
+    }
+}
