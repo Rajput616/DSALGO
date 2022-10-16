ff4a74e2abefe73dcb0932288d27c05810f042ec
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-16 15:34:28 +0530

    Time: 4 ms (69.09%), Space: 44.7 MB (31.48%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.java
@@ -19,18 +19,17 @@ class Solution {
         Map<Integer, Integer> iMap = new HashMap();
         for(int i = 0; i < len; ++i) iMap.put(inorder[i], i);
         
-        return constructTree(0, len-1, postorder, 0, len-1, inorder, iMap);
+        return constructTree(0, len-1, postorder, 0, iMap);
     }
-    private TreeNode constructTree(int postS, int postE, int[] pO, int inS, int inE, int[] in, 
-                                  Map<Integer, Integer> iMap){
-        if(postS > postE || inS > inE) return null;
+    private TreeNode constructTree(int postS, int postE, int[] pO, int inRoot, Map<Integer, Integer> iMap){
+        if(postS > postE) return null;
         
         TreeNode root = new TreeNode(pO[postE]);
         int index = iMap.get(pO[postE]);
-        int count = index - inS;
+        int count = index - inRoot;
         
-        root.left = constructTree(postS, postS+count-1, pO, inS, index-1, in, iMap);
-        root.right = constructTree(postS+count, postE-1, pO, index+1, inE, in, iMap);
+        root.left = constructTree(postS, postS+count-1, pO, inRoot, iMap);
+        root.right = constructTree(postS+count, postE-1, pO, index+1, iMap);
         return root;
     }
 }
