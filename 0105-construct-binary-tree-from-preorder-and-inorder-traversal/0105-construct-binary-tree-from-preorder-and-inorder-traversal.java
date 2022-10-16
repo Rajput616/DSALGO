d86f5887b42094bcde5c59cc162ca9711e0cc962
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-16 17:22:06 +0530

    Time: 4 ms (73.43%), Space: 44.2 MB (58.03%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,35 +1,21 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
+    int preS;
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         int len = inorder.length;
         Map<Integer, Integer> iMap = new HashMap();
         for(int i = 0; i < len; ++i) iMap.put(inorder[i], i);
-        
-        return constructTree(0, len-1, preorder, 0, iMap);
+        preS = 0;
+        return constructTree(0, len-1, preorder, iMap);
     }
-    private TreeNode constructTree(int preS, int preE, int[] pre, int inRoot, Map<Integer, Integer> iMap){
-        if(preS > preE) return null;
-        if(preS == preE) return new TreeNode(pre[preS]);
+    private TreeNode constructTree(int left, int right, int[] pre, Map<Integer, Integer> iMap){
+        if(left > right) return null;
         
         TreeNode root = new TreeNode(pre[preS]);
         int index = iMap.get(pre[preS]);
-        int count = index - inRoot;
-        root.left = constructTree(preS+1, preS + count, pre, inRoot, iMap);
-        root.right = constructTree(preS + count+1, preE, pre, index+1, iMap);
+        preS++;
+        root.left = constructTree(left, index-1, pre, iMap);
+        root.right = constructTree(index+1, right, pre, iMap);
         return root;
     }
 }
