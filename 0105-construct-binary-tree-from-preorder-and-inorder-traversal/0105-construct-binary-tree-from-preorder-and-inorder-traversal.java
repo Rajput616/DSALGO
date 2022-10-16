98972f3425d3ce051ef23efcb565b47b30cf624b
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-16 15:21:31 +0530

    Time: 2 ms (99.20%), Space: 42.2 MB (87.90%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,18 +19,17 @@ class Solution {
         Map<Integer, Integer> iMap = new HashMap();
         for(int i = 0; i < len; ++i) iMap.put(inorder[i], i);
         
-        return constructTree(0, len-1, preorder, 0, len-1, inorder, iMap);
+        return constructTree(0, len-1, preorder, 0, iMap);
     }
-    private TreeNode constructTree(int preS, int preE, int[] pre, int inS, int inE, int[] in,
-                                   Map<Integer, Integer> iMap){
-        if(preS > preE || inS > inE) return null;
+    private TreeNode constructTree(int preS, int preE, int[] pre, int inRoot, Map<Integer, Integer> iMap){
+        if(preS > preE) return null;
         if(preS == preE) return new TreeNode(pre[preS]);
         
         TreeNode root = new TreeNode(pre[preS]);
         int index = iMap.get(pre[preS]);
-        int count = index - inS;
-        root.left = constructTree(preS+1, preS + count, pre, inS, index-1, in, iMap);
-        root.right = constructTree(preS + count+1, preE, pre, index+1, inE, in, iMap);
+        int count = index - inRoot;
+        root.left = constructTree(preS+1, preS + count, pre, inRoot, iMap);
+        root.right = constructTree(preS + count+1, preE, pre, index+1, iMap);
         return root;
     }
 }
