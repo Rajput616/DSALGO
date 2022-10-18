8c9921c44a1efa1f45ba6cae3d47d6eff5ed3c19
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-18 19:35:04 +0530

    Time: 1 ms (74.63%), Space: 42 MB (65.95%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -1,36 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-        return makeBST(0, preorder.length-1, preorder);
-    }
-    private TreeNode makeBST(int i, int j, int[] preorder){
-        if(i > j) return null;
-        
-        TreeNode root = new TreeNode(preorder[i]);
-        int count = 0;
-        for(int k = i+1; k <= j; ++k){
-            if(preorder[k] > preorder[i]){
-                break;
-            }
-            count++;
-        }
-        
-        root.left = makeBST(i+1, i+count, preorder);
-        root.right = makeBST(i+count+1, j, preorder);
+        int len = preorder.length;
+        if(len == 0) return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        for(int i = 1; i < len; ++i) insert(preorder[i], root);
         return root;
+    }
+    private void insert(int val, TreeNode root){
+        if(root.val > val){
+            if(root.left == null) root.left = new TreeNode(val);
+            else insert(val, root.left);
+        } else{
+            if(root.right == null) root.right = new TreeNode(val);
+            else insert(val, root.right);
+        }
     }
 }
