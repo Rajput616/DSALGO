28f8c596b6990ae1e9edca872d5bcc6820187302
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-19 14:08:54 +0530

    Added solution - LeetHub

--- Floyd Warshall - GFG/floyd-warshall.java
+++ Floyd Warshall - GFG/floyd-warshall.java
@@ -45,7 +45,7 @@ class Solution
             for(int i = 0; i < n; ++i){
                 for(int j = 0; j < n; ++j){
                     if(matrix[i][k] == -1 || matrix[k][j] == -1) continue;
-                    if(matrix[i][j] == -1) matrix[i][j] = matrix[i][k] + matrix[k][j];
+                    else if(matrix[i][j] == -1) matrix[i][j] = matrix[i][k] + matrix[k][j];
                     else matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
                 }
             }
