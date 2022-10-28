c701551b6ab90548ca35561c94fddffbc5370a43
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-10-28 14:29:46 +0530

    Time: 12 ms (22.45%), Space: 57.8 MB (30.20%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int n = grid.length, m = grid[0].length;
+        int countIsland = 0;
+        boolean[][] vis = new boolean[n][m];
+        for(int i = 0; i < n; ++i){
+            for(int j = 0; j < m; ++j){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    markAllVis(i, j, grid, vis);
+                    countIsland++;
+                }
+            }
+        }
+        return countIsland;
+    }
+    
+    private void markAllVis(int i, int j, char[][] grid, boolean[][] vis){
+        if(i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || vis[i][j] || grid[i][j] == '0') return;
+        
+        vis[i][j] = true;
+        markAllVis(i-1, j, grid, vis);
+        markAllVis(i, j+1, grid, vis);
+        markAllVis(i+1, j, grid, vis);
+        markAllVis(i, j-1, grid, vis);
+    }
+}
