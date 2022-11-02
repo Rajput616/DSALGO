7dddea735a755ead848826b330508ac52cf677aa
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-11-02 16:21:47 +0530

    Time: 35 ms (29.28%), Space: 45.9 MB (89.35%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -12,6 +12,25 @@ class Solution {
         return dp[len1][len2];
     }
 }
+/* //Memoized
+    class Solution {
+    public int longestCommonSubsequence(String s, String t) {
+        int len1 = s.length();
+        int len2 = t.length();
+        return lcs(len1-1, len2-1, s, t, new int[len1][len2]);
+    }
+    private int lcs(int i, int j, String s, String t, int[][] dp){
+        if(i < 0 || j < 0) return 0;
+        if(dp[i][j] != 0) return dp[i][j];
+        
+        if(s.charAt(i) == t.charAt(j)) {
+            return dp[i][j] = 1 + lcs(i-1, j-1, s, t, dp);
+        } else{
+            return dp[i][j] = Math.max(lcs(i-1, j, s, t, dp), lcs(i, j-1, s, t, dp));
+        }
+    }
+}
+*/
 
 //Recursive
 /*
@@ -24,7 +43,7 @@ class Solution {
     private int lcs(int i, int j, String s, String t){
         if(i < 0 || j < 0) return 0;
         
-        if(s.charAt(i) == s.charAt(j)) {
+        if(s.charAt(i) == t.charAt(j)) {
             return 1 + lcs(i-1, j-1, s, t);
         } else{
             return Math.max(lcs(i-1, j, s, t), lcs(i, j-1, s, t));
@@ -33,22 +52,3 @@ class Solution {
 }
 */
 
-/* //Memoized
-    class Solution {
-    public int longestCommonSubsequence(String s, String t) {
-        int len1 = s.length();
-        int len2 = t.length();
-        return lcs(len1-1, len2-1, s, t, new int[len1][len2]);
-    }
-    private int lcs(int i, int j, String s, String t, int[][] dp){
-        if(i < 0 || j < 0) return 0;
-        
-        if(dp[i][j] != 0) return dp[i][j];
-        if(s.charAt(i) == t.charAt(j)) {
-            return dp[i][j] = 1 + lcs(i-1, j-1, s, t, dp);
-        } else{
-            return dp[i][j] = Math.max(lcs(i-1, j, s, t, dp), lcs(i, j-1, s, t, dp));
-        }
-    }
-}
-*/
