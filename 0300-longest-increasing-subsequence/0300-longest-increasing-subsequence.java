c6654a0ff1c1aa10dc9135c37b7218a4890ff9de
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2022-11-03 14:43:15 +0530

    Time: 4 ms (97.24%), Space: 44.5 MB (45.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,18 +1,36 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        
-        int[] dp = new int[n];
-        Arrays.fill(dp, 1);
-        
-        int maxLen = 1;
+        int n = nums.length, len = 0;
+        int[] arr = new int[n];
+        arr[len++] = nums[0];
         for(int i = 1; i < n; ++i){
-            for(int j = 0; j <= i; ++j){
-                if(nums[j] < nums[i] && dp[i] < 1 + dp[j])
-                    dp[i] = 1 + dp[j];
+            if(nums[i] < arr[0]) arr[0] = nums[i];
+            else if(nums[i] > arr[len-1]) arr[len++] = nums[i];
+            else {
+                int index = Arrays.binarySearch(arr, 0, len, nums[i]);
+                if(index < 0) index = -index-1;
+                arr[index] = nums[i];
             }
-            maxLen = Math.max(maxLen, dp[i]);
         }
-        return maxLen;        
+        return len;
     }
 }
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n = nums.length;
+        
+//         int[] dp = new int[n];
+//         Arrays.fill(dp, 1);
+        
+//         int maxLen = 1;
+//         for(int i = 1; i < n; ++i){
+//             for(int j = 0; j <= i; ++j){
+//                 if(nums[j] < nums[i] && dp[i] < 1 + dp[j])
+//                     dp[i] = 1 + dp[j];
+//             }
+//             maxLen = Math.max(maxLen, dp[i]);
+//         }
+//         return maxLen;        
+//     }
+// }
