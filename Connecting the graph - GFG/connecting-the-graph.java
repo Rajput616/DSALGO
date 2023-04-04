ec6384be2d4256da90edd498d0ccff9b0a8b693e
Saurabh Rajput <75022816+Rajput616@users.noreply.github.com>
2023-04-04 11:31:13 +0530

    Added solution - LeetHub

--- Connecting the graph - GFG/connecting-the-graph.java
+++ Connecting the graph - GFG/connecting-the-graph.java
@@ -9,15 +9,15 @@ class GFG {
         int T = sc.nextInt();
         while (T-- > 0) {
             int n = sc.nextInt();
-            int m=sc.nextInt();
+            int m = sc.nextInt();
             int[][] edge = new int[m][2];
-            for(int i=0;i<m;i++){
-                edge[i][0]=sc.nextInt();
-                edge[i][1]=sc.nextInt();
+            for (int i = 0; i < m; i++) {
+                edge[i][0] = sc.nextInt();
+                edge[i][1] = sc.nextInt();
             }
-            
+
             Solution obj = new Solution();
-            int ans = obj.Solve(n,edge);
+            int ans = obj.Solve(n, edge);
             System.out.println(ans);
         }
     }
@@ -25,16 +25,18 @@ class GFG {
 
 // } Driver Code Ends
 
+
 class DSU{
-    int[] parent, rank;
+    int[] rank, parent;
+    
     DSU(int n){
-        parent = new int[n];
         rank = new int[n];
+        parent = new int[n];
         for(int i = 0; i < n; ++i) parent[i] = i;
     }
     
     public int findPar(int u){
-        if(u == parent[u]) return u;
+        if(parent[u] == u) return u;
         return parent[u] = findPar(parent[u]);
     }
     
@@ -53,20 +55,37 @@ class DSU{
 }
 
 class Solution {
-    
     public int Solve(int n, int[][] edges) {
         DSU dsu = new DSU(n);
+        int minEdgesReqd = -1;
         int extraEdges = 0;
-        for(int[] edge : edges){
-            int u = edge[0], v = edge[1];
-            if(dsu.findPar(u) != dsu.findPar(v)){
-                dsu.union(u, v);
-            } else extraEdges++;
+        for(int[] e : edges){
+            int u = e[0];
+            int v = e[1];
+            if(dsu.findPar(u) != dsu.findPar(v)) dsu.union(u, v);
+            else extraEdges++;
         }
-        int minEdgesReq = -1; 
+        
         for(int i = 0; i < n; ++i){
-            if(dsu.findPar(i) == i) minEdgesReq++;
+            if(dsu.findPar(i) == i) minEdgesReqd++;
         }
-        return extraEdges >= minEdgesReq ? minEdgesReq : -1;
+        
+        if(minEdgesReqd > extraEdges) return -1;
+        return minEdgesReqd;
+        
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
